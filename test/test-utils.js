ad2f367c129ecb34c2a9a53067814885acd04027
Alex Blair <bclan11@gmail.com>
2021-12-09 11:54:40 -0800

    tests working

--- test/test-utils.js
+++ test/test-utils.js
@@ -2,17 +2,37 @@ import {
     renderFriend,
     renderMushroom
  } from '../render-utils.js';
- 
+
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test("This function should return a div of class='friend', a p of class='name' with the value of friend.name, and a p of class='emoji with an emoji face correspondent to the satisfaction levels of 1, 2, and 3.", (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+
+    const friend = {
+        name: 'Fred',
+        satisfaction: 2
+    }
+    const expected = '<div class="friend"><p class="name">Fred</p><p class="emoji">😑</p></div>';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderFriend(friend).outerHTML;
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test("This function should return a div element of class='mushroom'", (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+
+    const expected = '<div class="mushroom"></div>';
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = renderMushroom().outerHTML;
 
     //Expect
     // Make assertions about what is expected versus the actual result
